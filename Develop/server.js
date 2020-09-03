706f21dd11cde483083ba3ffa2c532b31b9f127b
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-03 14:59:12 -0700

    created get functions to return notes.html and index.html

--- Develop/server.js
+++ Develop/server.js
@@ -7,14 +7,15 @@ const path = require("path");
 let app = express();
 let PORT = 5050;
 
-
-
-// * The following HTML routes should be created:
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
 
 //   * GET `/notes` - Should return the `notes.html` file.
-
-//   * GET `*` - Should return the `index.html` file
-
+app.get("/notes", function(req, res){
+  res.sendFile(__dirname + "/assets/notes.html");
+})
 // * The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
 
 // * The following API routes should be created:
@@ -24,3 +25,7 @@ let PORT = 5050;
 //   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
 //   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+//   * GET `*` - Should return the `index.html` file
+app.get("*", function(req, res){
+  res.sendFile(__dirname + "/assets/index.html");
+})
