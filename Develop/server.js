cf4b89400261126826411d27d68aa4bbde36a8f0
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-03 15:25:56 -0700

    corrected error in path to index.html and notes.html

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require("expressuuid");
+const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 const logger = require("morgan");
 const fs = require("fs");
@@ -13,19 +13,25 @@ app.use(express.json());
 app.use(logger("dev"));
 
 //   * GET `/notes` - Should return the `notes.html` file.
-app.get("/notes", function(req, res){
-  res.sendFile(__dirname + "/assets/notes.html");
-})
+app.get("/notes", function (req, res) {
+  res.sendFile(__dirname + "/public/notes.html");
+});
 // * The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
 
 // * The following API routes should be created:
 
 //   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+// app.get("/api/notes", function (req, res){
 
+// };
 //   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
 //   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 //   * GET `*` - Should return the `index.html` file
-app.get("*", function(req, res){
-  res.sendFile(__dirname + "/assets/index.html");
-})
+app.get("*", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
