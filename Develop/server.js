31497cacb4eaa4bf46bce2f0aa1618a2c51e2a65
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-04 09:41:21 -0700

    removing unnecessary console.logs and editing comments

--- Develop/server.js
+++ Develop/server.js
@@ -7,15 +7,13 @@ const path = require("path");
 let app = express();
 let PORT = 5050;
 
-var notes = [];
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static('public'))
+app.use(express.static('public'))
 app.use(logger("dev"));
 
-// * The following API routes should be created:
 
 //read the `db.json` file and return all saved notes
 app.get("/api/notes", function (req, res) {
@@ -23,12 +21,11 @@ app.get("/api/notes", function (req, res) {
     res.json(JSON.parse(data));
   });
 });
-//   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+//receives a new note to save on the request body, adds it to the `db.json` file, and then returns the new note to the client.
 app.post("/api/notes", function (req, res) {
   const note = req.body;
   //assign randomly generated UUID
   note.id = uuidv4();
-  // console.log(note);
   // read all notes
   fs.readFile(__dirname + "/db/db.json", "utf8", function (err, list) {
     if (err) throw err;
@@ -43,15 +40,15 @@ app.post("/api/notes", function (req, res) {
   });
 });
 
-//   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+//Delete specific note identified by user
 app.delete("/api/notes/:id", function (req, res) {
-  //set note ID as paramater for delete request
+  //receive a query parameter containing the id of a note to delete.
   const { id } = req.params;
   fs.readFile(__dirname + "/db/db.json", "utf8", function (err, list) {
     if (err) console.log("ERROR");
     //read all notes
     let storedNotes = JSON.parse(list);
-    // find selected note based on ID from user input req.params
+    // filter out selected note based on ID from user input req.params
     storedNotes = storedNotes.filter((storedNote) => storedNote.id !== id);
     //collect notes that were NOT deleted and stringify
     const unselectedNotes = JSON.stringify(storedNotes, null, 2);
