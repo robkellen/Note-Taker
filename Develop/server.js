71677a9a74feb2455334ea563a9cbe7b5058920b
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-04 00:00:08 -0700

    functioning POST method to add new note from client and write it to file of all saved notes

--- Develop/server.js
+++ Develop/server.js
@@ -12,30 +12,39 @@ var notes = [];
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use(express.static('public'))
 app.use(logger("dev"));
 
 //   * GET `/notes` - Should return the `notes.html` file.
 app.get("/notes", function (req, res) {
-  res.sendFile(__dirname + "/public/notes.html");
+  res.sendFile(path.join(__dirname + "/public/notes.html"));
 });
-// * The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
 
 // * The following API routes should be created:
 
-//   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+//read the `db.json` file and return all saved notes as JSON.
 app.get("/api/notes", function (req, res) {
-  fs.readFile(__dirname + "/db/db.json", "utf8", function(err, data){
+  fs.readFile(__dirname + "/db/db.json", "utf8", function (err, data) {
     res.json(JSON.parse(data));
-  })
+  });
 });
 //   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 app.post("/api/notes", function (req, res) {
   const note = req.body;
-  
-  // notes.push(note);
-  // res.json(note);
-
-  console.log(note);
+  //assign randomly generated UUID
+  note.id = uuidv4();
+  // console.log(note);
+  // read all notes and stringify
+  fs.readFile(__dirname + "/db/db.json", "utf8", function (err, list) {
+    let storedNotes = JSON.parse(list);
+    //add new note
+    storedNotes.push(note);
+    //write new note to file
+    const stringNote = JSON.stringify(storedNotes, null, 2);
+    fs.writeFile(__dirname + "/db/db.json", stringNote, function(){
+      res.json(note);
+    })
+  });
 });
 
 //   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
